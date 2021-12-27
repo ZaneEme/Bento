8f907adda11bfce843b0f23e52684a0a921209f5
Zane Emerick <zaneemerick@gmail.com>
2021-12-27 10:53:07 -0700

    customized lists and blocks to fit my needs

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ const CONFIG = {
   weatherUnit: 'F', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
-  trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+  trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
   defaultLatitude: '38.843241',
   defaultLongitude: '-104.761298',
 
@@ -52,56 +52,52 @@ const CONFIG = {
     },
     {
       id: '3',
+      name: 'Outlook',
+      icon: 'send',
+      link: 'https://outlook.office365.com/mail/',
+    },
+    {
+      id: '4',
       name: 'Todoist',
       icon: 'trello',
       link: 'https://todoist.com',
     },
     {
-      id: '4',
+      id: '5',
       name: 'Calendar',
       icon: 'calendar',
       link: 'https://calendar.google.com/calendar/r',
     },
     {
-      id: '5',
-      name: 'Reddit',
-      icon: 'glasses',
-      link: 'https://reddit.com',
-    },
-    {
       id: '6',
-      name: 'Odysee',
-      icon: 'youtube',
-      link: 'https://odysee.com/',
-    },
+      name: 'Canvas',
+      icon: 'award',
+      link: 'https://canvas.uccs.edu',
+    },  
   ],
 
   // ┬  ┬┌─┐┌┬┐┌─┐
   // │  │└─┐ │ └─┐
   // ┴─┘┴└─┘ ┴ └─┘
-/*
+
   //Icons
-  firstListIcon: 'music',
-  secondListIcon: 'coffee',
+  firstListIcon: 'pen-tool',
+  secondListIcon: 'briefcase',
 
   // Links
   lists: {
     firstList: [
       {
-        name: 'Inspirational',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'UCCS',
+        link: 'https://www.uccs.edu',
       },
       {
-        name: 'Classic',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'Outlook',
+        link: 'https://outlook/office365.com/mail',
       },
       {
-        name: 'Oldies',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-      },
-      {
-        name: 'Rock',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'Canvas',
+        link: 'https://canvas.uccs.edu',
       },
     ],
     secondList: [
@@ -110,17 +106,9 @@ const CONFIG = {
         link: 'https://linkedin.com/',
       },
       {
-        name: 'Figma',
-        link: 'https://figma.com/',
-      },
-      {
-        name: 'Dribbble',
-        link: 'https://dribbble.com',
-      },
-      {
-        name: 'Telegram',
-        link: 'https://webk.telegram.org',
+        name: 'Indeed',
+        link: 'https://indeed.com/',
       },
     ],
-  },*/
+  },
 };
